bcdfc121d2304b4bfe3e65df222f8b4c8174c803
xavilopez <xavi_lopez011@hotmail.com>
2017-02-10 15:19:04 -0300

    modal to viewImage

--- js/fito.js
+++ js/fito.js
@@ -4,9 +4,15 @@ $(function() {
     var context = $(eTarget).context;
     var parent = $(context).closest('.recordCard');
     var str = $(parent).find('small').text();
+
+    _openModal(str);
   })
 
+  function _openModal(str){
+    $('#flipFlop').modal('show');
+  }
 
+  
 
 
 });
