e45342c135141d3f0f57aaf83408566208d8a453
xavilopez <xavi_lopez011@hotmail.com>
2017-02-10 11:40:51 -0300

    link go to viewImage

--- js/fito.js
+++ js/fito.js
@@ -3,12 +3,10 @@ $(function() {
     var eTarget =  $(e.target).find('.recordDetail');
     var context = $(eTarget).context;
     var parent = $(context).closest('.recordCard');
-    console.log($(parent).find('small').text());
-
+    var str = $(parent).find('small').text();
   })
 
-  $('.recordCard').on('click', (e)=>{
-    $('.progressText').text('NIVEL 2');
-  })
+
+
 
 });
