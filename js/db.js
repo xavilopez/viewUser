7129597fcd368a662e375e5af1dea7147ee6b703
xavilopez <xavi_lopez011@hotmail.com>
2017-02-15 16:40:50 -0300

    test10

--- js/db.js
+++ js/db.js
@@ -1,12 +1,7 @@
 var fbUserId= '';
-var carlblack = (function(fbUserId){
-  return {
-      call: function() {
-          console.log(fbUserId);
-        }
-      };
-})(fbUserId);
 
 
 $(function() {
+
+
 });
