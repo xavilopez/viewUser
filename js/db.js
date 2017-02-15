2606d768a46bccca1f6748f02bf723c8400ad169
xavilopez <xavi_lopez011@hotmail.com>
2017-02-15 16:53:07 -0300

    selectUser

--- js/db.js
+++ js/db.js
@@ -1,5 +1,11 @@
 var fbUserId= '';
-
+var carlblack = (function(fbUserId){
+  return {
+      call: function() {
+          console.log(fbUserId);
+        }
+      };
+})(fbUserId);
 
 $(function() {
 
