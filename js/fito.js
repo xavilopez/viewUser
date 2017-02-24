67dd26d6661bf1d2789dd00322fa1f861ff08f49
xavilopez <xavi_lopez011@hotmail.com>
2017-02-24 10:21:48 -0300

    card detail test console log

--- js/fito.js
+++ js/fito.js
@@ -12,14 +12,24 @@ $(function() {
     var link;
     if (str == "curvybenchpress") {
       link = "https://xavilopez.github.io/curvybenchpress/";
+      console.log(str);
+
     }else if (str == "curvylegcurls") {
       link = "https://xavilopez.github.io/curvybenchpress/";
+      console.log(str);
+
     }else if (str == "curvywomenbackexercises") {
       link = "https://xavilopez.github.io/curvywomenback/";
+      console.log(str);
+
     }else if (str == "declinebenchpress") {
       link = "https://xavilopez.github.io/declinebenchpress/";
+      console.log(str);
+
     }else if (str == "dumbbell") {
       link = "https://xavilopez.github.io/dumbbell/";
+      console.log(str);
+
     }
     $('.viewImageFrame').attr('src', link);
     $('#flipFlop').modal('show');
