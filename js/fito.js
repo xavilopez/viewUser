33a38d96068c5dbd4a5858727c9d27b32accd701
xavilopez <xavi_lopez011@hotmail.com>
2017-02-24 09:38:17 -0300

    console log detailcard test

--- js/fito.js
+++ js/fito.js
@@ -4,11 +4,12 @@ $(function() {
     var context = $(eTarget).context;
     var parent = $(context).closest('.recordCard');
     var str = $(parent).find('small').text();
-
+    console.log($(str).trim().toUpper());
     _openModal(str);
   })
 
   function _openModal(str){
+
     $('#flipFlop').modal('show');
   }
 
